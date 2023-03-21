44e9836f06e09159e4a0098499891faec5ef350a
FearosD <fearos@valar.ru>
2023-03-21 16:49:24 +0300

    test public 2

--- src/js/helpers/assets-list.js
+++ src/js/helpers/assets-list.js
@@ -1,7 +1,4 @@
 export const assetsImagesURL = [
-  'https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg',
-  'https://images.pexels.com/photos/719396/pexels-photo-719396.jpeg',
-  'https://wallpapercave.com/wp/wp2670841.jpg',
   './assets/images/dungeon/dungeon-map.png',
   './assets/images/characters/merchant/marchant-idle.png',
   './assets/images/characters/player/player-normal/player-idle.png',
